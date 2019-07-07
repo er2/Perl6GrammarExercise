8de55400767414b83eb4589abd3cb0ff20c8867d
Eric Riese <eric.riese@gmail.com>
2019-07-07 02:08:15 -0400

    much shorter, no warnings

--- lib/Compression.pm
+++ lib/Compression.pm
@@ -5,31 +5,26 @@ use v6;
 unit module Compression;
 
 grammar Compression {
-   token TOP           { <term>+ }
-   token term          { <letters> | <compressed> }
-   token compressed    { <emptybrackets> | <letterset> }
-   token letterset     { <number> '[' <TOP> ']' }
-   token letters       { <[a..z]>+ }
-   token number        { \d+ }
-   token emptybrackets { "[]" }
+   token TOP       { <term>+ }
+   token term      { <letters> | <letterset> }
+   token letterset { <number>? '[' <TOP>? ']' }
+   token letters   { <[a..z]>+ }
+   token number    { \d+ }
 }
 
 class Decompress {
-         method TOP($/)                               { make [~] $<term>.map: *.made }
-   multi method term($/ where $<letters>)             { make $<letters>.made }
-   multi method term($/ where $<compressed>)          { make $<compressed>.made }
-   multi method compressed($/ where $<emptybrackets>) { make $<emptybrackets>.made }
-   multi method compressed($/ where $<letterset>)     { make $<letterset>.made }
-         method letterset($/)                         { make $<TOP>.made x +$<number> }
-         method letters($/)                           { make ~$/ }
-         method emptybrackets($/)                     { "" }
+         method TOP($/)                     { make [~] $<term>.map: *.made }
+   multi method term($/ where $<letters>)   { make $<letters>.made }
+   multi method term($/ where $<letterset>) { make $<letterset>.made }
+         method letterset($/)               { make ($<TOP>.made // "") x +($<number> // 0) }
+         method letters($/)                 { make ~$/ }
 }
 
 sub parse($corpus) is export {
    Compression.parse($corpus, actions => Decompress.new).made;
 }
 
-my @examples = "3[a]","3[abc]4[ab]c", "a[]b";
+my @examples = "3[a]","3[abc]4[ab]c","a[]b";
 
 for @examples -> $example {
    say "$example => {parse($example)}";
